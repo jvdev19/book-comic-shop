9495de35964498f5d3fa7999ec77232f204c6031
jvdev19 <koutt26@gmail.com>
2023-02-02 17:20:09 +0100

    Realizando pruebas para renderizar el grid

--- src/app/Ui.js
+++ src/app/Ui.js
@@ -1,10 +1,17 @@
 // 1. Ahora estamos poniendo esta clase genérica para toda la interfaz de usuario. Iremos pasando como parámetro al constructor los elementos de interfaz de usuario. Si hay demasiados, habría que separar esta clase, en varias, que representen los elementos de la interfaz.
 
 export class Ui {
-    constructor(container){
+    constructor(container) {
         this.container = document.querySelector(container);
     }
-    renderData(data){
-       this.container.innerHTML += `<p>${data}</p> <br>`;
+    createGrid() {
+        this.container.innerHTML +=`
+            <div class="product-grid"></div>
+        `;
+    }
+    renderProducts(container, products, ...productData) {
+        products.forEach(product => {
+            container.appendChild(product[productData]);
+        });
     }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -4,15 +4,18 @@ const { Ui } = require('./Ui');
 
 // -->> El fichero index.js siempre será el fichero que se encargará de arrancar la app en nuestro entorno de desarrollo. Al compilar para producción, veremos cómo se genera una carpeta /dist, con un bundle.js donde irá todo el código compilado.
 
-const book1 = new Book('Carrie', 'Stephen King', 20);
+const bookCarrie = new Book('Carrie', 'Stephen King', 20);
+const comicTheKillingJoke = new Comic('The Killing Joke', 'Alan Moore', 150, ['B.B','J.H'] );
+const products = [bookCarrie, comicTheKillingJoke];
 const ui = new Ui("#app");
-const comic1 = new Comic('The Killing Joke', 'Alan Moore', 150, ['B.B','J.H'] );
 
 (function () {
-    let bookData = book1.getAllData();
-    let comicData = comic1.getAllData();
-    ui.renderData(bookData);
-    ui.renderData(comicData);
+    ui.createGrid();
+    products.forEach(product => {
+        let productData = product.getAllData();
+        console.log(productData);
+    });
+
 })();
 
 
